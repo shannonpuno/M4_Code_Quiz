4ddca36786e9ec8c282a65b60a45620104c9bc8d
Shannon Alegre <shannonalegre@Shannons-MacBook-Pro.local>
2022-10-05 12:12:50 -0700

    added starter code

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+//Write variables 
+var startButton = document.getElementById("startQuiz");
+var scoreResults = document.getElementById("scores");
+var displayTimer = document.getElementById("timer");
+
+
+//Declare Variables
+
+//Function to display quiz questions
+
+//Function to view High Scores Page
+
+//Function for Start Quiz when start button is clicked
+
+// Function to display score 
+
+//Timer function when start button is clicked 
+
+//Add function to add User Intials and Submit Button
